a6773413a1a42e88dc64c06f5484891f21f8cd12
prince-edu <edubish16@gmail.com>
2023-11-28 20:23:25 -0600

    login corregido

--- app/src/main/java/com/example/imhungry/MainActivity.java
+++ app/src/main/java/com/example/imhungry/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.imhungry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.imhungry.gui.LogInActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, LogInActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/imhungry/gui/LogInActivity.java
+++ app/src/main/java/com/example/imhungry/gui/LogInActivity.java
@@ -2,7 +2,14 @@ package com.example.imhungry.gui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.imhungry.R;
 
@@ -12,5 +19,41 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        Log.d("ActivityMain", "Hola");
+
+        Button buttonLogin = findViewById(R.id.button_login);
+        buttonLogin.setOnClickListener(v->{
+            Log.d("ActivityMain", "haciendio click");
+
+                EditText usuarioET = findViewById(R.id.text_view_username);
+                EditText passwordET = findViewById(R.id.text_view_password);
+
+                String usuario = usuarioET.getText().toString();
+                String password = passwordET.getText().toString();
+
+                if(TextUtils.isEmpty(usuario)){
+
+                    Toast.makeText(LogInActivity.this, "Debes ingresar un usuario", Toast.LENGTH_SHORT).show();
+
+                }else if(TextUtils.isEmpty(password)){
+
+                    Toast.makeText(LogInActivity.this, "Debes ingresar la contraseña", Toast.LENGTH_SHORT).show();
+
+                }else{
+
+                    if(usuario.equals("Rau") && password.equals("123")){
+
+                        Toast.makeText(LogInActivity.this, "Exito", Toast.LENGTH_SHORT).show();
+
+
+                    }else{
+
+                        Toast.makeText(LogInActivity.this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+        });
+
     }
 }
